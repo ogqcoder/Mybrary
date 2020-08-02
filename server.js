373b774094ffacc77912c22b4beb795b8abf9d2d
ogqcoder <gartho77@gmail.com>
2020-08-02 19:43:19 -0400

    changed db_url to DATABASE_URL

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const indexRouter = require("./routes/index");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
