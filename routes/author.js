da9afdbb32f5ac0213b83c7500795728af2194c0
ogqcoder <gartho77@gmail.com>
2020-08-04 01:11:57 -0400

    author routes

---
+++ routes/author.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const Author = require("../models/authors");
+
+//all authors
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", { authors: authors, searchOptions: req.query });
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+//new author
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+router.post("/", async (req, res) => {
+  //   res.render("authors/new");
+  const author = new Author({
+    name: req.body.name,
+  });
+
+  try {
+    const newAuthor = await author.save();
+    //   res.redirect(`authors/${newAuthor.id}`);
+    res.redirect(`authors`);
+  } catch (error) {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating Author",
+    });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,18 +5,28 @@ const express = require("express");
 const app = express();
 const indexRouter = require("./routes/index");
 const mongoose = require("mongoose");
+const authorRouter = require("./routes/author");
+const expressLayouts = require("express-ejs-layouts");
+// const { applyEachSeries } = require("async");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.set("view engine", "ejs");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
 app.set(express.static("public"));
 
+app.use(express.urlencoded({ extended: false }));
 app.use("/", indexRouter);
 // app.get("/", (req, res) => {
 //   res.render("beast");
 // });
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
